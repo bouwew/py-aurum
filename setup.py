bb040053d1643b48dee93b5d7ee662a1a784afed
Bouwe Westerdijk <11290930+bouwew@users.noreply.github.com>
2020-04-13 17:18:43 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='py_aurum',
+    version='0.0.1',
+    description='Aurum API to use in conjunction with Home Assistant Core.',
+    long_description='Aurum Meetstekker API to use in conjunction with Home Assistant Core.',
+    keywords='Home Assistant HA Core Aurum',
+    url='https://github.com/bouwew/py_aurum',
+    author='@bouwew',
+    author_email='bouwe.s.westerdijk@gmail.com',
+    license='MIT',
+    packages=['py_aurum'],
+    install_requires=['requests','lxml'],
+    zip_safe=False
+)

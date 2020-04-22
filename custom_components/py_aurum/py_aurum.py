ac3a28b4e0f3927819062149bb69c41a3a9edf9b
Bouwe Westerdijk <11290930+bouwew@users.noreply.github.com>
2020-04-22 15:37:17 +0200

    Create py_aurum.py

---
+++ custom_components/py_aurum/py_aurum.py
@@ -0,0 +1,53 @@
+"""Aurum Core module."""
+import requests
+import logging
+from lxml import etree
+
+AURUM_DATA = "/measurements/output.xml"
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Aurum:
+    """Define the Aurum object."""
+
+    def __init__(self, host, port=80):
+        """Set the constructor for this class."""
+        self._session = requests.Session()
+        self._endpoint = 'http://' + host + ':' + str(port)
+
+    def collect_data(self):
+        data = {}
+        xml = None
+        url = self._endpoint + AURUM_DATA
+
+        try:
+            xml = self._session.get(url)
+            xml.raise_for_status()
+        except requests.exceptions.HTTPError as errh:
+            _LOGGER.error("Http Error:", errh)
+        except requests.exceptions.ConnectionError as errc:
+            _LOGGER.error("Error Connecting:", errc)
+        except requests.exceptions.Timeout as errt:
+            _LOGGER.error("Timeout Error:", errt)
+        except requests.exceptions.RequestException as err:
+            _LOGGER.error("OOps: Something Else", err)
+
+        if xml is not None:
+            result = xml.text
+            root = etree.fromstring(result)
+            idx = 1
+            for item in root:
+                sensor = item.tag
+                value = item.get("value")
+                if sensor != "smartMeterTimestamp":
+                    if sensor == "powerElectricity":
+                        value = int(float(value))
+                    elif abs(float(value)) > 10:
+                        value = float("{:.1f}".format(round(float(value), 1)))
+                    else:
+                        value = float("{:.2f}".format(round(float(value), 2)))
+                    
+                data[idx] =  {sensor: value}
+                idx += 1
+            return data

0d1b639b7b8d56398fde984dca14b62e90941f22
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-05-02 16:59:03 +0200

    Import async code

--- build/lib/py_aurum/py_aurum.py
+++ build/lib/py_aurum/py_aurum.py
@@ -1,9 +1,14 @@
 """Aurum Core module."""
-import requests
+import asyncio
 import logging
 from lxml import etree
 
+import aiohttp
+import async_timeout
+
 AURUM_DATA = "/measurements/output.xml"
+
+DEFAULT_TIMEOUT = 20
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -11,43 +16,70 @@ _LOGGER = logging.getLogger(__name__)
 class Aurum:
     """Define the Aurum object."""
 
-    def __init__(self, host, port=80):
+    def __init__(
+        self,
+        host,
+        port=80,
+        timeout=DEFAULT_TIMEOUT,
+        websession=None,
+    ):
         """Set the constructor for this class."""
-        self._session = requests.Session()
-        self._endpoint = 'http://' + host + ':' + str(port)
 
-    def collect_data(self):
+        if websession is None:
+
+            async def _create_session():
+                return aiohttp.ClientSession()
+
+            loop = asyncio.get_event_loop()
+            self.websession = loop.run_until_complete(_create_session())
+        else:
+            self.websession = websession
+
+        self._endpoint = 'http://' + host + ':' + str(port)
+        self._timeout = timeout
+
+    async def collect_data(self, retry=2):
+        """Connect to the Aurum meetstekker."""
+        # pylint: disable=too-many-return-statements
         data = {}
-        xml = None
         url = self._endpoint + AURUM_DATA
 
         try:
-            xml = self._session.get(url)
-            xml.raise_for_status()
-        except requests.exceptions.HTTPError as errh:
-            _LOGGER.error("Http Error:", errh)
-        except requests.exceptions.ConnectionError as errc:
-            _LOGGER.error("Error Connecting:", errc)
-        except requests.exceptions.Timeout as errt:
-            _LOGGER.error("Timeout Error:", errt)
-        except requests.exceptions.RequestException as err:
-            _LOGGER.error("OOps: Something Else", err)
+            with async_timeout.timeout(self._timeout):
+                resp = await self.websession.get(url)
+        except (asyncio.TimeoutError, aiohttp.ClientError):
+            if retry < 1:
+                _LOGGER.error("Error connecting to the Aurum meetstekker", exc_info=True)
+                raise self.ConnectionFailedError
+            return await self.connect(retry - 1)
 
-        if xml is not None:
-            result = xml.text
-            root = etree.fromstring(result)
-            idx = 1
-            for item in root:
-                sensor = item.tag
-                value = item.get("value")
-                if sensor != "smartMeterTimestamp":
-                    if sensor == "powerElectricity":
-                        value = int(float(value))
-                    elif abs(float(value)) > 10:
-                        value = float("{:.1f}".format(round(float(value), 1)))
-                    else:
-                        value = float("{:.2f}".format(round(float(value), 2)))
-                    
-                data[idx] =  {sensor: value}
-                idx += 1
-            return data
+        result = await resp.text()
+        _LOGGER.debug("Collecting data from the Aurum meetstekker: %s", result)
+        root = etree.fromstring(result)
+        idx = 1
+        for item in root:
+            sensor = item.tag
+            value = item.get("value")
+            if sensor != "smartMeterTimestamp":
+                if sensor == "powerElectricity":
+                    value = int(float(value))
+                elif abs(float(value)) > 10:
+                    value = float("{:.1f}".format(round(float(value), 1)))
+                else:
+                    value = float("{:.2f}".format(round(float(value), 2)))
+                
+            data[idx] =  {sensor: value}
+            idx += 1
+        return data
+
+    async def close_connection(self):
+        """Close the Aurum connection."""
+        await self.websession.close()
+
+
+    class AurumError(Exception):
+        """Aurum exceptions class."""
+
+
+    class ConnectionFailedError(AurumError):
+        """Raised when unable to connect."""

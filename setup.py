2d02d2b4451b1bf98f7f08632a6162f0642b0092
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-05-16 15:07:21 +0200

    Take version nr from __init__.py

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
+import codecs
+import os.path
 from setuptools import setup
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 def readme():
     with open('README.rst') as f:
@@ -6,7 +21,7 @@ def readme():
 
 setup(
     name='py_aurum',
-    version='0.1.8',
+    version=get_version("py_aurum/__init__.py"),
     description='Aurum API to use in conjunction with Home Assistant Core.',
     long_description='Aurum Meetstekker API to use in conjunction with Home Assistant Core.',
     keywords='Home Assistant HA Core Aurum',
